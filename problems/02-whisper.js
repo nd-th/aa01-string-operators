3186ef6ad988a8198173d510bc5b0c456b6da55d
Ndeye <ndeyet2004@gmail.com>
2024-05-14 11:43:59 -0700

    Done with String homework

--- problems/02-whisper.js
+++ problems/02-whisper.js
@@ -3,10 +3,13 @@ Define a function whisper that takes in a string parameter and returns a
 "whispered" version of that string.
 */
 
-// Your code here 
+// Your code here
+function whisper(str){
+    return `...${str.toLowerCase()}...`;
+}
 
-// console.log(whisper("Hey Buddy")); // "...hey buddy..."
-// console.log(whisper("YEA! that was FUN")); // "...yea! that was fun..."
+console.log(whisper("Hey Buddy")); // "...hey buddy..."
+console.log(whisper("YEA! that was FUN")); // "...yea! that was fun..."
 
 /******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/
 module.exports = whisper;
